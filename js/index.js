d7946a54ef4598da9b22daa53841217bde663c4a
Tim Bingtan <timbingtan@gmail.com>
2023-03-20 15:12:20 -0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// var startingPosition = $(".cloud").css("left");
+// alert(startingPosition);
+
+//$(".cloud").animate({left: '2000px'}, 10000);
+
+
+// Changing navbar color based on scroll area
+document.addEventListener('scroll', () => {
+  document.documentElement.dataset.scroll = window.scrollY;
+});
+
+//Test with one list element first. The first online
+// let blogItem = $("a .life-blog");
+// console.log(blogItem);
+
+// let listItems = $("li a");
+// console.log(listItems.length);
+// for (let i = 0; i < listItems.length; i++){
+//   listItems[i].addEventListener('mouseover', (event) => {
+//     event.target.parentElement.style.margin = "auto auto -1rem";
+//     console.log("got here");
+//   })
+// }
